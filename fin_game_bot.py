ac5afa0e345c3e6363bbdfcca8135d6c38451a96
Elvira-Zainulina <Elvira-Zainulina@users.noreply.github.com>
2020-09-13 03:52:28 +0500

    bot

--- fin_game_bot.py
+++ fin_game_bot.py
@@ -8,15 +8,17 @@ import json
 
 
 class FinGameBot(Bot):
-    _quiz_data = None
+    # _quiz_data = None
     _quiz_sequence = None
     _round_sequence = None
     _cur_question = 0
+    _cur_block = 0
     # _story = []
 
     def __init__(self, bot_token: str, data_pth: str):
         super(FinGameBot, self).__init__(bot_token, data_pth)
-        self._quiz_data = self.read_data("quiz")
+        # self._quiz_data = self.read_data("quiz")
+        self._quiz_sequence = QuizSequence("data/quiz_data.json")
 
     def read_data(self, key: str):
         self._quiz_sequence = QuizSequence("data/quiz_data.json")
@@ -80,7 +82,7 @@ class FinGameBot(Bot):
 
     @staticmethod
     def quiz_keyboard2(options, right_answer):
-        answers = ['right' if (right_answer - 1) == i else 'wrong' for i in range(2)]
+        answers = ['right' if (i + 1) in right_answer else 'wrong' for i in range(2)]
         keyboard = [[InlineKeyboardButton(options[0], callback_data=answers[0])],
                     [InlineKeyboardButton(options[1], callback_data=answers[1])],
                     [InlineKeyboardButton("Завершить игру", callback_data='end')]]
@@ -112,9 +114,9 @@ class FinGameBot(Bot):
         context.bot.send_message(chat_id=update.effective_chat.id, text=help_msg)
 
     def image(self, update, context):
-        image_path = './QR-code_telegram.png'
+        img_path = self._quiz_sequence.get_block(3).get_advice_pic()
         context.bot.send_photo(update.effective_chat.id,
-                               photo=open(image_path, 'rb'))
+                               photo=open(img_path, 'rb'))
 
     # @staticmethod
     def start(self, update, context):
@@ -138,32 +140,47 @@ class FinGameBot(Bot):
                                   reply_markup=self.quiz_start_keyboard())
 
     def quiz(self, update, context):
-        test = self._quiz_data._blocks[0]
-        query = update.callback_query
+        test = self._quiz_sequence
+        # test = self._quiz_data._blocks[0]
         # test = self._quiz_data
-        if self._cur_question >= len(test):
-            self._cur_question = 0
+        if self._cur_block > self._quiz_sequence.get_sequence_size():
+            self._cur_block = 0
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text="Новых вопросов нет. Готов ли ты к повторению?",
                                      reply_markup=self.quiz_start_keyboard())
             return
 
-        question_obj = test[self._cur_question]
+        question_obj = test.get_question(self._cur_block, self._cur_question)
         question = question_obj.get_text()
         options = question_obj.get_vars()
         right_answer = question_obj.get_true()
-        self._cur_question += 1
 
         context.bot.send_message(chat_id=update.effective_chat.id, text=question,
                                  reply_markup=self.quiz_keyboard2(options, right_answer))
 
+    def update_cur_position(self, update, context):
+        block = self._quiz_sequence.get_block(self._cur_block)
+        print(self._cur_question, self._cur_block, block.get_block_size())
+        if self._cur_question < block.get_block_size():
+            self._cur_question += 1
+        else:
+            advice = block.get_advice()
+            picture = block.get_advice_pic()
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=advice)
+            context.bot.send_photo(chat_id=update.effective_chat.id,
+                                   photo=open(picture, 'rb'))
+            self._cur_block += 1
+            self._cur_question = 0
+
     def right(self, update, context):
-        question_obj = self._quiz_data._blocks[0][self._cur_question - 1]
+        question_obj = self._quiz_sequence.get_question(self._cur_block, self._cur_question)
         congrats = question_obj.get_text() + '\n'
-        ans = question_obj.get_vars()[question_obj.get_true() % 2]
+        ans = question_obj.get_vars()[question_obj.get_true()[0] % 2]
         congrats += f'Правильно. {ans}. Explanation.'
         # self._story.append([question_obj.get_text(),
         #                     question_obj.get_vars()[question_obj.get_true()]])
+        self.update_cur_position(update, context)
         query = update.callback_query
         context.bot.edit_message_text(chat_id=query.message.chat_id,
                                       message_id=query.message.message_id,
@@ -171,12 +188,13 @@ class FinGameBot(Bot):
                                       reply_markup=self.quiz_next_keyboard())
 
     def wrong(self, update, context):
-        question_obj = self._quiz_data._blocks[0][self._cur_question - 1]
+        question_obj = self._quiz_sequence.get_question(self._cur_block, self._cur_question)
         explanation = question_obj.get_text() + '\n'
-        ans = question_obj.get_vars()[question_obj.get_true() % 2]
+        ans = question_obj.get_vars()[question_obj.get_true()[0] % 2]
         explanation += f'К сожалению, ответ "{ans}" неправильный. \n' + 'Explanation.'
         # self._story.append([question_obj.get_text(),
         #                     question_obj.get_vars()[question_obj.get_true() % 2]])
+        self.update_cur_position(update, context)
         query = update.callback_query
         context.bot.edit_message_text(chat_id=query.message.chat_id,
                                       message_id=query.message.message_id,

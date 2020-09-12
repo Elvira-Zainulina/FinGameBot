ecebc3a62592a1b05cbd59945175aa23b21fe76f
Valery Ilin <valery.ilin@skoltech.ru>
2020-09-12 18:36:43 +0300

    types on lists

--- utils/structures/variant.py
+++ utils/structures/variant.py
@@ -3,5 +3,11 @@ class Variant(object):
         self._answer = data["answer"]
         self._description = data["description"]
 
+    def get_variant_answer(self) -> str:
+        return self._answer
+
+    def get_variant_description(self) -> str:
+        return self._description
+
     def __repr__(self):
         return self._answer
